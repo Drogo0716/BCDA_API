b908fbd7cfa223e6e85a2bdc42b61bf04d44c7fe
Drogo0716 <denvernhce@gmail.com>
2021-04-13 10:13:48 -0500

    Minor changes to Production_API.py

--- shelvingForEoB.py
+++ shelvingForEoB.py
@@ -3,6 +3,7 @@ import shelve
 shelfFile = shelve.open('shelf_For_EoB_Key_Separation')
 shelfFile['init'] = True
 shelfFile['write'] = False
+shelfFile['index'] = 0
 print(list(shelfFile.values()))
 print(list(shelfFile.keys()))
 #print(shelfFile['init'])
